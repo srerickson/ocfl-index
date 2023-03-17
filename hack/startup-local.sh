95edc47612ac81103f36fe0f196baee701117c97
Seth Erickson <sr.erickson@gmail.com>
2023-03-17 15:38:54 -0400

    add local startup script example

---
+++ hack/startup-local.sh
@@ -0,0 +1,32 @@
+# startup example
+app="cmd/ocfl-index/ocfl-index"
+
+# storage backend type: "fs", "s3", or "azure"
+export OCFL_INDEX_BACKEND="fs"  
+
+# path relative to bucket/fs to OCFL storage root
+export OCFL_INDEX_STOREDIR="testdata/simple-root"
+
+# path to index file
+export OCFL_INDEX_SQLITE="simpleroot.sqlite"
+
+# number of go routines for object scan
+export OCFL_INDEX_SCANWORKERS=100
+
+# number of go routeins for inventory parse
+export OCFL_INDEX_PARSEWORKERS=6
+
+
+# Additional options:
+
+# server port ("localhost:8080")
+# export OCFL_INDEX_LISTEN
+
+ # concurrency level (default to # of processors)
+# export OCFL_INDEX_MAXGOROUTINES
+
+# alternative s3 endpoint
+# export AWS_S3_ENDPOINT
+
+# start server and index filesizes
+$($app server $@)
